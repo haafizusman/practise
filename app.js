f69d4f15458a14d488951f593301ace244597cd8
haafizusman <121035086+haafizusman@users.noreply.github.com>
2022-12-19 21:39:03 +0500

    yes
    yes

---
+++ app.js
@@ -0,0 +1,218 @@
+// alert("hello wrold !")
+
+// console.log("hello world !")
+
+// var a = "mani";
+
+// alert (a)
+
+// var name;
+
+// name = "kami"
+
+// alert (name)
+
+// var num = 256;
+
+// alert (num)
+
+// var num;
+
+// num = 54
+
+// alert (num)
+
+// var name@ = "mani"
+
+// var name = "mani";
+
+// var mat = 2+2
+
+// alert (mat)
+
+// var num1 = 2
+
+// var num2 = 4
+
+// add = num1 + num2
+
+// alert (add)
+
+// var num1 = 2
+
+// var add = num1 + 8
+
+// alert (add)
+
+// var num = 2
+
+// var num2 = ++num
+
+// alert (num2)
+
+// var num = 2
+
+// var num1 = num + 3 * 4
+
+// alert (num1)
+
+// var pro = "thanx";
+
+// alert (pro + " " + "mani")
+
+// var a = prompt()
+
+// alert (a)
+
+// var a = "mani"
+
+// if (a === "man"){
+//     alert ("thanx mani")
+// }else {
+//     alert ("not")
+// }
+
+// var a = "immi"
+
+// if (a === "mani" || a === "kami"){
+//     alert ("family")
+// }else {
+//     alert ("not")
+// }
+
+// var a = prompt()
+
+// if (a === "mani"){
+//     alert ("thanx usman")
+// }else if (a === "kami"){
+//     alert("thanx kamran")
+// }else {
+//     alert ("not")
+// }
+
+// var age = prompt()
+
+// if ( age < 20 && age > 10){
+//     alert ("equal")
+// }else {
+//     alert ("not equal")
+// }
+
+// var arr = ["mani","kami"]
+
+// arr[2] = "imi"
+
+// console.log(arr)
+
+// var arr = [2,4,5,6]
+
+// arr.shift() //remove from starting
+
+// arr.unshift(8) adding to start
+
+// arr.splice(2,0, "mani") adding in between
+
+// var a = arr.slice(2,3) copy paste (length count)
+
+// arr.pop() delete last
+
+// arr.push("mani") add in last
+
+// console.log(arr)
+// console.log(a)
+
+// for (var i = 10 ; i > 0 ; i--){
+//     console.log(i)
+// }
+
+// var a = ["usman","kamran"]
+
+// var b = ["siddiqui"]
+
+// for (var i = 0 ; i < 1 ; i++){
+
+//     for (var j = 0 ; j < b.length ; j++){
+//         console.log(a + b)
+//     }
+    
+// }
+// var name = prompt("enter your name")
+
+// var name2 = name.toLowerCase()
+
+// if (name2 === "mani"){
+//     alert ("thanx")
+// }
+
+// var a = "mani"
+
+// var name = a.slice(0,1)
+
+// console.log(name)
+
+// var name = "mani"
+
+// var name1 = name.charAt(1)
+
+// console.log(name1)
+
+// var text = "my name is mani"
+
+// console.log(text)
+
+// var change = text.replace("mani","usman")
+
+// console.log(change)
+
+// var round = 2.3
+
+// var comp = Math.round(round)
+
+// console.log(round)
+// console.log(comp)
+
+// var num = 2.6
+
+// var upper = Math.ceil(num)
+
+// console.log(num)
+// console.log()
+
+// var num = 2.9
+
+// var neeche = Math.floor(num)
+
+// console.log(num)
+
+// console.log(neeche)
+
+// console.log(Math.random())
+
+// var num = 15
+
+// console.log(num)
+
+//  var a = num.toString()
+
+// console.log(a)
+
+// var name = "mani"
+
+// console.log(name)
+
+// var change = Number(name)
+
+
+// console.log(change)
+
+
+
+
+
+
+
+
+
+
+
+
